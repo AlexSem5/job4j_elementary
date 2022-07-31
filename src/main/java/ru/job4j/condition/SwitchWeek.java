54dfac9ed28de001637b69ab54ee7f583c570c08
Aleksandr Semeikin <sensem@yandex.ru>
2022-07-31 14:10:04 +0300

    4.6.1. Switch [#334070]

---
+++ src/main/java/ru/job4j/condition/SwitchWeek.java
@@ -0,0 +1,75 @@
+package ru.job4j.condition;
+
+public class SwitchWeek {
+    public static String nameOfDay(int day) {
+        String name;
+        switch (day) {
+            case 1:
+                name = "Понедельник";
+                break;
+            case 2:
+                name = "Вторник";
+                break;
+            case 3:
+                name = "Среда";
+                break;
+            case 4:
+                name = "Четверг";
+                break;
+            case 5:
+                name = "Пятница";
+                break;
+            case 6:
+                name = "Суббота";
+                break;
+            case 7:
+                name = "Воскресенье";
+                break;
+            default:
+                name = "Ошибка";
+                break;
+        }
+        return name;
+    }
+
+    public static String nameOfDay2(int day) {
+        String name;
+        switch (day) {
+            case 1 -> name = "Понедельник";
+            case 2 -> name = "Вторник";
+            case 3 -> name = "Среда";
+            case 4 -> name = "Четверг";
+            case 5 -> name = "Пятница";
+            case 6 -> name = "Суббота";
+            case 7 -> name = "Воскресенье";
+            default -> name = "Ошибка";
+        }
+        return name;
+    }
+
+    public static String nameOfDay3(int day) {
+        String name = switch (day) {
+            case 1 -> "Понедельник";
+            case 2 -> "Вторник";
+            case 3 -> "Среда";
+            case 4 -> "Четверг";
+            case 5 -> {
+                System.out.println("Hello Friday");
+                yield "Пятница";
+            }
+            case 6 -> "Суббота";
+            case 7 -> "Воскресенье";
+            default -> "Ошибка";
+        };
+        return name;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(nameOfDay(10));
+        System.out.println(nameOfDay2(10));
+        System.out.println(nameOfDay3(5));
+    }
+}
+
+
+
