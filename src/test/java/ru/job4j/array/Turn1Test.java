08ff985268a6d658086479acbbcdfe0a710ed3da
Aleksandr Semeikin <sensem@yandex.ru>
2022-08-02 15:34:15 +0300

    6.2.3. Перевернуть массив. [#226790] New

---
+++ src/test/java/ru/job4j/array/Turn1Test.java
@@ -0,0 +1,25 @@
+package ru.job4j.array;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Turn1Test {
+
+    @Test
+    public void whenTurnArrayWithEvenAmountOfElementsThenTurnedArray() {
+        int[] input = new int[]{4, 1, 6, 2};
+        int[] result = Turn.back(input);
+        int[] expected = new int[]{2, 6, 1, 4};
+        assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    public void whenTurnArrayWithOddAmountOfElementsThenTurnedArray() {
+        int[] input = new int[]{3, 1, 7, 2, 5};
+        int[] result = Turn.back(input);
+        int[] expected = new int[]{5, 2, 7, 1, 3};
+        assertThat(result).containsExactly(expected);
+    }
+}
