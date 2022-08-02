199c6d7d246999abc9d3cf9f4bd6f1492e4806cb
Aleksandr Semeikin <sensem@yandex.ru>
2022-08-02 20:45:04 +0300

    6.4.2. Обход массива с последнего элемента [#361619]

---
+++ src/main/java/ru/job4j/array/RollBackArray.java
@@ -0,0 +1,15 @@
+package ru.job4j.array;
+
+import java.util.Arrays;
+
+public class RollBackArray {
+    public static int[] rollback(int[] array) {
+        int[] result = new int[array.length];
+        for (int index = 0; index < array.length; index++) {
+            int lastIndex = array.length - 1;
+            int currentIndex = index;
+            result[currentIndex] = array[lastIndex - currentIndex];
+        }
+        return result;
+    }
+}
