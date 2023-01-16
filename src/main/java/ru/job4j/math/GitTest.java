eb36eb2e8cca1e644b584c12a8d5243e1e33bb02
Aleksandr Semeikin <sensem@yandex.ru>
2023-01-16 20:58:09 +0300

    new one instead of "first"

--- src/main/java/ru/job4j/math/GitTest.java
+++
@@ -1,7 +0,0 @@
-package ru.job4j.math;
-
-public class GitTest {
-    public static void main(String[] args) {
-        System.out.printf("GitTest111");
-    }
-}
---
+++ src/main/java/ru/job4j/math/Test.java
@@ -0,0 +1,2 @@
+package ru.job4j.math;public class Test {
+}
