a3d89b253c230b6247ff4c1e8dccbefe55e2bac0
Aleksandr Semeikin <sensem@yandex.ru>
2023-01-16 18:08:45 +0300

    New test

--- src/main/java/ru/job4j/math/GitTest.java
+++ src/main/java/ru/job4j/math/GitTest.java
@@ -2,6 +2,6 @@ package ru.job4j.math;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.printf("GitTest");
+        System.out.printf("GitTest1");
     }
 }
